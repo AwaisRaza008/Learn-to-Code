9b39f5655e4d641f72910208641262cb36ed983a
AwaisRaza008 <101279231+AwaisRaza008@users.noreply.github.com>
2022-10-11 01:00:15 -0700

    Update ReverseNumber.java

--- Java/ReverseNumber.java
+++ Java/ReverseNumber.java
@@ -1,6 +1,9 @@
+import java.util.*;
 public class ReverseNumber {
     public static void main(String[] args) {
-        int num = 987654321;
+        Scanner sc=new scanner (System.in);
+        System.out.print("Enter a number : ");
+        int num =sc.nextInt();
         int rem=0 , rev=0;
         while(num!=0) {
             
@@ -8,6 +11,6 @@ public class ReverseNumber {
                 rev = rev * 10 + rem;
                 num = num/ 10;
         }
-        System.out.println("The Reverse number is: " + rev);
+        System.out.printf("\nThe Reverse number is : ",rev);
     }
 }
